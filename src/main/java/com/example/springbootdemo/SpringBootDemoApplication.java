cfdf0b1f446dfd7f8b32e61a7c9813402da1ba4a
sjx <1105648611@qq.com>
2021-12-12 21:56:30 +0800

    wu

--- src/main/java/com/example/springbootdemo/SpringBootDemoApplication.java
+++ src/main/java/com/example/springbootdemo/SpringBootDemoApplication.java
@@ -2,7 +2,8 @@ package com.example.springbootdemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+// https://blog.csdn.net/u011863024/article/details/113667634
+// https://blog.csdn.net/u011863024/article/details/113667946
 @SpringBootApplication
 public class SpringBootDemoApplication {
 
